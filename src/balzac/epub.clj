c3be9176b2b94cd6ba236e9b1be48578b597840a
tomob <yubizume@gmail.com>
2013-05-27 23:35:45 +0200

    Epubs handled with Book protocol

--- src/balzac/epub.clj
+++ src/balzac/epub.clj
@@ -1,17 +1,30 @@
 (ns balzac.epub
-  (:use ([clojure.java.io]))
-  (:import [nl.siegmann.epublib.epub EpubReader]))
+  (:use [clojure.java.io]
+        [balzac.proto])
+  (:import [nl.siegmann.epublib.epub EpubReader]
+           [nl.siegmann.epublib.domain Identifier]))
 
+(defrecord Epub [meta]
+  Book
+  (authors [book] (map str (.getAuthors meta)))
+  (title [book] (first (.getTitles meta)))
+  (language [book] (.getLanguage meta))
+  (publication-date [book] (first (.getDates meta)))
+  (isbn [book]
+    (let [bookid (.getValue (Identifier/getBookIdIdentifier (.getIdentifiers meta)))]
+      (if (.contains bookid ":")
+        (last (.split bookid ":"))
+        bookid))))
 
 (defn epub
   "Parses all headers of an .epub file."
   [is]
   (let [er (EpubReader.)
         book (.readEpub er is)]
-    (.getMetadata book)))
+    (Epub. (.getMetadata book))))
 
 
 ;; Helpers for REPL
 
-(defn is [] (input-stream "/Users/tomo/Desktop/epub/Metro_2033.epub"))
-(defn is2 [] (input-stream "/Users/tomo/Desktop/epub/Real_World_Haskell.epub"))
+(defn is1e [] (input-stream "/Users/tomo/Desktop/epub/Metro_2033.epub"))
+(defn is2e [] (input-stream "/Users/tomo/Desktop/epub/Real_World_Haskell.epub"))
